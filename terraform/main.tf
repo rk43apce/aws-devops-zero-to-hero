8a0ba15155bd381be3f03c7727390f8b8b97e8c6
Rajesh Kumar <rk43apce@gmail.com>
2026-03-19 22:14:28 +0530

    adding day files

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "web_sg" {
+  name = "web_sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0f58b397bc5c1f2e8" # Amazon Linux (update if needed)
+  instance_type = "t2.micro"
+
+  security_groups = [aws_security_group.web_sg.name]
+
+  key_name = var.key_name
+
+  user_data = <<-EOF
+            #!/bin/bash
+            yum update -y
+            yum install docker -y
+            systemctl start docker
+            systemctl enable docker
+            usermod -a -G docker ec2-user
+            EOF
+
+  tags = {
+    Name = "cicd-demo"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,3 @@
+variable "key_name" {
+  description = "terraform-ec2-key"
+}
